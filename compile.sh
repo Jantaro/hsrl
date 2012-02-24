cb1e2e5738dc5bac06ec4b2c2fda584d1031c51c
Sam Stepanian <jantaro6@gmail.com>
2012-02-24 18:08:27 -0500

    'actually' fixed compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -O2 -Wall -Wextra -std=c++0x -pedantic -lsfml-graphics -lsfml-window -lsfml-system hsrl.cpp -o hsrl
+g++ hsrl.cpp -o hsrl -O2 -Wall -Wextra -std=c++0x -pedantic -lsfml-graphics -lsfml-window -lsfml-system
